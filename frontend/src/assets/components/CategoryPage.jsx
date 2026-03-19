146ebdfc5672cd912c217b27b0d971b6fb02fa00
Neeraj saini <ndmark007@gmail.com>
2026-03-19 20:11:28 +0530

    Fix API reference error in CategoryPage.jsx

--- frontend/src/assets/components/CategoryPage.jsx
+++ frontend/src/assets/components/CategoryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import axios from "axios";
+import API from "../api/api";
 import Nev from "./Nev";
 import Footer from "./Footer";
 import { FaBoxes } from "react-icons/fa";
